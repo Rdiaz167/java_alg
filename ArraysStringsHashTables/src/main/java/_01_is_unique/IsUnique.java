ff84f49ce67f7a97f159c7fa5a83a413a3d34de9
Roberto <rd.navaro@gmail.com>
2024-10-29 20:07:35 -0300

    isuniqueproblem

--- ArraysStringsHashTables/src/main/java/_01_is_unique/IsUnique.java
+++ ArraysStringsHashTables/src/main/java/_01_is_unique/IsUnique.java
@@ -2,4 +2,17 @@ package _01_is_unique;
 
 public class IsUnique {
 
+    private static final int NUMBER_OF_CHARS = 128;
+
+    public boolean isUnique(String s) {
+
+        if(s.length() > NUMBER_OF_CHARS) return false;
+
+        Set<Character> characters = new HashSet<>();
+        for(Character c : s.toCharArray()) {
+            if(characters.contains(c)) return false;
+            characters.add(c);
+        }
+        return true;
+    }
 }
